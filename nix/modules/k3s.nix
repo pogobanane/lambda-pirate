7c66bd3cf05ad49e668af70d727eaa470db3c115
Jörg Thalheim <joerg@thalheim.io>
2021-06-01 08:28:08 +0200

    k3s: upstream containerd extensions
    fixup! k3s: upstream containerd extensions

--- nix/modules/k3s.nix
+++ nix/modules/k3s.nix
@@ -20,17 +20,8 @@ let
       }
     ];
   };
-  settingsFormat = pkgs.formats.toml {};
-  hasZfs = lib.any (fs: fs == "zfs") config.boot.supportedFilesystems;
 in
 {
-  options.virtualisation.containerd.settings = lib.mkOption {
-    type = settingsFormat.type;
-    default = "";
-    description = ''
-      Verbatim lines to add to containerd.toml
-    '';
-  };
   config = {
     environment.systemPackages = [
       (pkgs.runCommand "wrap-kubectl"
@@ -47,34 +38,21 @@ in
     services.k3s.docker = false;
     networking.firewall.allowedTCPPorts = [ 6443 ];
 
-    # Upstream this?
-    virtualisation.containerd.configFile = settingsFormat.generate "containerd.toml"
-      config.virtualisation.containerd.settings;
-
     virtualisation.containerd.enable = true;
 
     virtualisation.containerd.settings = {
-      # Upstream this?
-      plugins.cri.containerd.snapshotter = lib.mkIf hasZfs "zfs";
-      plugins.cri.cni = {
-        bin_dir = "${pkgs.cni-plugins}/bin";
-        conf_dir = "${pkgs.writeTextDir "net.d/10-flannel.conflist" flannel}/net.d";
-      };
+      plugins.cri.cni.conf_dir = "${pkgs.writeTextDir "net.d/10-flannel.conflist" flannel}/net.d";
     };
 
-    systemd.services.containerd.serviceConfig = lib.mkIf hasZfs {
+    systemd.services.containerd.serviceConfig = lib.mkIf config.boot.zfs.enabled {
       ExecStartPre = "-${pkgs.zfs}/bin/zfs create -o mountpoint=/var/lib/containerd/io.containerd.snapshotter.v1.zfs zroot/containerd";
     };
-
-    # Upstream this?
-    systemd.services.containerd.path = lib.mkIf hasZfs [ pkgs.zfs ];
 
     services.k3s.role = "server";
     services.k3s.extraFlags = toString [
       "--disable traefik"
       "--disable metrics-server"
       "--flannel-backend=host-gw"
-      "--kubelet-arg=cgroup-driver=systemd"
       "--container-runtime-endpoint unix:///etc/firecracker-containerd/fccd-cri.sock"
     ];
   };
