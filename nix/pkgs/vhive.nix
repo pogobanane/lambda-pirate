b10c37a02be98ba2b9f489115bb7b219a74af330
Jörg Thalheim <joerg@thalheim.io>
2021-06-01 18:30:04 +0200

    vhive: increase vm start timeout

--- nix/pkgs/vhive.nix
+++ nix/pkgs/vhive.nix
@@ -17,10 +17,12 @@ buildGoModule rec {
   doCheck = false;
 
   patches = [
+    # available in next release
     (fetchpatch {
       url = "https://github.com/ease-lab/vhive/commit/db5cac4a1a25f17229bd26af84503ec235a14707.patch";
       sha256 = "sha256-Wqc6DKhDPK+1JmuWEiq0bxFJxWScoj247aaNPDLOA4Q=";
     })
+    ./0001-increase-vm-start-timeout.patch
   ];
 
   vendorSha256 = "sha256-LsXL2jS/w5fES8lX9n2rbQtgVjuleNwQjVNd+Xjp5TI=";
