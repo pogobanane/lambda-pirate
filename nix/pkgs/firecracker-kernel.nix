cf16f0fc44d9a5757a3504053abbf6dc0549f74a
Pogobanane <aenderboy@gmx.de>
2021-08-30 12:12:46 +0000

    try newer linux. Does this work?

--- nix/pkgs/firecracker-kernel.nix
+++ nix/pkgs/firecracker-kernel.nix
@@ -1,9 +1,9 @@
-{ stdenv, buildLinux, fetchurl, firecracker, linux_4_14 }:
+{ stdenv, buildLinux, fetchurl, firecracker, linux }:
 # `buildLinux` is way to fucking complex, let's just write it from scratch
 stdenv.mkDerivation rec {
   # this version seems to be what firecracker is currently using:
   # https://github.com/firecracker-microvm/firecracker/blob/81cd08af07e91deca20acf0f576ca3e84ce8b2a9/src/vmm/src/utilities/mock_resources/make_noisy_kernel.sh#L15
-  inherit (linux_4_14)
+  inherit (linux)
     name src version buildInputs nativeBuildInputs depsBuildBuild;
 
   postPatch = ''
@@ -17,7 +17,8 @@ stdenv.mkDerivation rec {
   postConfigure = ''
     cp ${firecracker.src}/resources/microvm-kernel-x86_64.config .config
     # true is for broken pipe
-    (yes \n || true) | make oldconfig
+    make oldconfig
+    #(yes \n || true) | make oldconfig
   '';
 
   installPhase = ''
