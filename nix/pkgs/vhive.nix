cf4ba389648f345f86fe323ee83fad710bd9d936
Pogobanane <aenderboy@gmx.de>
2021-09-01 10:58:18 +0000

    update vhive version

--- nix/pkgs/vhive.nix
+++ nix/pkgs/vhive.nix
@@ -4,14 +4,16 @@ buildGoModule rec {
   pname = "vhive";
   #version = "v" + "1.2";
   #rev = "v${version}";
-  version = "peterTag";
+  #rev = "fb7971ce539dd6ce8dd81a42d70dc5a53833c858";
+  version = "pogobanane-fork";
   #ldflags = "-w";
 
   # not fetchFromGitHub to skip git-lfs assets
   src = fetchgit {
     url = "https://github.com/pogobanane/vhive";
-    rev = "${version}";
-    sha256 = "sha256-P7QtHVXvXZwsWhVAy2hE6/KKZ14bVPBeBc3yT8+PUDY=";
+    rev = "8ca309b856e47448907292955f32af6ecad148c6";
+    sha256 = null;
+    #sha256 = "sha256-4WGmW+HiRfvnNqiGndBGucyDPY3W3XDDPuadtNa6gOM=";
   };
   # The following src is quite useless, because:
   # - it only works with --impure
@@ -40,10 +42,10 @@ buildGoModule rec {
     #   sha256 = "sha256-Wqc6DKhDPK+1JmuWEiq0bxFJxWScoj247aaNPDLOA4Q=";
     # })
     # https://github.com/ease-lab/vhive/pull/265
-    (fetchpatch {
-      url = "https://github.com/ease-lab/vhive/commit/c71594795c5cccd0d1c432ff6819048dab966c86.patch";
-      sha256 = "sha256-a1Q81zcI5+6/nHTznUxVGabX6dCFNDzofrV2ijiVmoY=";
-    })
+    #(fetchpatch {
+      #url = "https://github.com/ease-lab/vhive/commit/c71594795c5cccd0d1c432ff6819048dab966c86.patch";
+      #sha256 = "sha256-a1Q81zcI5+6/nHTznUxVGabX6dCFNDzofrV2ijiVmoY=";
+    #})
 
     #./0001-increase-vm-start-timeout.patch
     ##./0001-set-upper-limit-for-autoscaling.patch
@@ -52,6 +54,11 @@ buildGoModule rec {
     #./0001-limit-autoscaling-min.patch
     #./0001-debug-print-guest-IPs.patch
   ];
+
+  patchPhase = ''
+    # we now use vendored/peter which is already vendored
+    #go mod vendor
+  '';
 
   vendorSha256 = null;
   #vendorSha256 = "sha256-zDXhO7uDbPLdr6RYFiGJp3AZXGj7B8nIXCrdWRJVQkM=";
