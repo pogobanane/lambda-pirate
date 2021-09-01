6f1fbf6af7e79be17115d41ebf7e9441cbb9881b
Pogobanane <aenderboy@gmx.de>
2021-09-01 10:27:58 +0000

    add python3 logfmt

--- flake.nix
+++ flake.nix
@@ -41,6 +41,9 @@
             pkgs.delve
             pkgs.lsof
           ];
+          packageOverrides = pkgs.callPackage ./nix/python-packages.nix { };
+          python = pkgs.python3.override { inherit packageOverrides; };
+          pythonWithPackages = python.withPackages (ps: [ ps.logfmt ]);
         in
         {
           packages = rec {
@@ -78,7 +81,9 @@
               ownPkgs.istioctl
               ownPkgs.kn
               rustToolchain
-            ];
+              pythonWithPackages
+            ]
+            ;
             shellHook = ''
               if [ -n $KUBECONFIG ]; then
                 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
