70537d4f8396324caaad9e4b639647a86618784c
Jörg Thalheim <joerg@thalheim.io>
2022-01-28 16:45:05 +0100

    switch to nixpkgs istio

--- nix/pkgs/istioctl.nix
+++ nix/pkgs/istioctl.nix
@@ -3,23 +3,24 @@
 buildGoModule rec {
   pname = "istioctl";
   # latest version still fails
-  version = "1.7.1";
+  version = "1.12.2";
   nativeBuildInputs = [ go-bindata ];
 
   src = fetchFromGitHub {
     owner = "istio";
     repo = "istio";
     rev = version;
-    sha256 = "sha256-CvYqYxb19lSc61SrvGcf/kG9e9zosHz4JbByA+bN600=";
+    sha256 = "sha256-6eVFyGVvOUr5RA5jeavKcLJedv4jOGXAg3aa4N3cNx8=";
   };
 
   # Bundle charts
-  preBuild = ''
-    patchShebangs operator/scripts
-    operator/scripts/create_assets_gen.sh
-  '';
+  #preBuild = ''
+  #  patchShebangs operator/scripts
+  #  bin/update_crds.sh
+  #  operator/scripts/create_assets_gen.sh
+  #'';
 
   subPackages = [ "istioctl/cmd/istioctl" ];
   doCheck = false;
-  vendorSha256 = "sha256-4Z4Fgv9zmAwp3tEvHj8yLOWkFY/zFz5VfehSWCuIUcI=";
+  vendorSha256 = "sha256-4Z4Fgv9zmAwp3tEvHj8yLOWkFY/zFz5VfehSWCuIU0I=";
 }
